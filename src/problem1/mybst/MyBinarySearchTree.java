5a0e75ea7bb16b34b843af26a8d1ea63a65d05aa
mukund agrawal <mukund.agrawal_cs18@gla.ac.in>
2020-03-28 16:01:13 +0530

    fields created of MyBinarySearchTree class

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,9 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+import problem1.node.TreeNode;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    TreeNode root;
+    int count;
 }
