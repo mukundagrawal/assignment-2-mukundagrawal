c0ad7124eff917d94333364b3343e9c2b566ee41
mukund agrawal <mukund.agrawal_cs18@gla.ac.in>
2020-03-28 17:10:25 +0530

    getter and setter created of problem 3

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,25 @@
  */
 package problem3.node;
 
+import problem5.student.Student;
+
 public class Node {
+    private Student student;
+    private Node nextNode;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
 }
